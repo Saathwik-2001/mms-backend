b55797178c1ae2ff34a9cab8d955d93e8e58b6f6
Saathwik <sathwik.galimutti@gmail.com>
2023-07-14 13:46:16 +0530

    added bp.json()

--- Server.js
+++ Server.js
@@ -15,6 +15,7 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api/users",userRoute);
 app.use("/api/blogs", blogRoute);
